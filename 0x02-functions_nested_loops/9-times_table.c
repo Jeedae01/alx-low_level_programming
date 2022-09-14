5af3af9771ddee2cf03b4742aaea34a042508084
Jeedae01 <idrisbabajide7@gmail.com>
2022-09-13 17:33:26 -0700

    Thank GOD

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -5,17 +5,11 @@
  * Return: Always 0.
  */
 
-
 void times_table(void)
-
 
 {
 	int a;
-
-
 	int b;
-
-
 	int c;
 
 	for (a = 0; a <= 9; a++)
@@ -23,7 +17,7 @@ void times_table(void)
 		for (b = 0; b <= 9; b++)
 		{
 			c = a * b;
-		if ((c / 10) == 0)
+			if ((c / 10) == 0)
 			{
 				if (b == 0)
 				{
@@ -32,27 +26,21 @@ void times_table(void)
 				if (b != 0)
 				{
 					_putchar (' ');
-
 					_putchar ((c % 10) + '0');
 				}
 				if (b < 9)
 				{
 					_putchar(',');
-
 					_putchar (' ');
 				}
 			}
 			else
 			{
-
 				_putchar ((c / 10) + '0');
-
 				_putchar ((c % 10) + '0');
-
 				if (b < 9)
 				{
 					_putchar(',');
-
 					_putchar (' ');
 				}
 			}
