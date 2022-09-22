4969737d66151c15ff77023b3c349a87eb2629e1
Jeedae01 <idrisbabajide7@gmail.com>
2022-09-22 16:24:26 -0700

    GOD

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -7,6 +7,7 @@
  *   @r: result
  *   @size_r: result lenght
  *   Return: sum
+ *
  */
 
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
@@ -17,26 +18,25 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 	int i = 0, j = 0, k, l = 0, f, s, d = 0;
 
 	while (n1[i] != '\0')
+
 		i++;
 	while (n2[j] != '\0')
+
 		j++;
 	if (i > j)
 		l = i;
 	else
 		l = j;
 	if (l + 1 > size_r)
-
 		return (0);
-
 	r[l] = '\0';
-
 	for (k = l - 1 ; k >= 0 ; k--)
 	{
 		i--;
+
 		j--;
 
 		if (i >= 0)
-
 			f = n1[i] - '0';
 		else
 			f = 0;
