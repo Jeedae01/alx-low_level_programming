33a037e1ab3f462e9f607d313e6bb9f61b9baeca
Jeedae01 <idrisbabajide7@gmail.com>
2022-10-12 21:42:30 +0000

    God, updated functions

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,12 +1,12 @@
 #include "function_pointers.h"
 
 /**
- * print_name - takes name and print function to print name
- * @name: name to print
- * @f: pointer to the function to print name
- * Return: void
-*/
-
+ * print_name - prints a name.
+ * @name: input name.
+ * @f: function pointer.
+ *
+ * Return: no return.
+ */
 void print_name(char *name, void (*f)(char *))
 {
 	if (name && f)
